9ef1a314957dbabe09de3ffbc38d6da1c13624b0
JS <kubasitnik79@gmail.com>
2022-02-14 16:57:14 +0100

    partly configured webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,56 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  entry: {
-    index:'./src/index.js'
-  },
-  mode: 'development',
-  output: {
-    filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(csv|tsv)$/i,
-        use: ['csv-loader'],
-      },
-      {
-        test: /\.xml$/i,
-        use: ['xml-loader'],
-      },
+module.exports = (env) => {
+  console.log(env.goal);
+  console.log(env.production);
+  
+  return {
+    mode: 'development',
+    entry: {
+      index:'./src/index.js',
+      print:'./src/print.js',
+    },
+    // not for production!!!
+    devtool: 'inline-source-map',
+    devServer: {
+      static: './dist',
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Development',
+      }),
     ],
-  },
+    output: {
+      filename: '[name].[contenthash].js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
+    },
+    module: {
+      rules: [
+        //TypeScript
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+        //CSS
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        //Images
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+    optimization: {
+      moduleIds: 'deterministic',
+    },
+  };
 };
