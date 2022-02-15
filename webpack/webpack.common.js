cf538860bc759b92e3d9117b96eced88e327bde5
JS <kubasitnik79@gmail.com>
2022-02-15 18:39:26 +0100

    Repair webpack work with casual formats of files

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -4,65 +4,64 @@ const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 module.exports = {
     entry:  path.resolve(__dirname, '../src/index.ts'),
-    plugins: [
-      new HtmlWebpackPlugin({
-        title: 'Production',
-      }),
-      new ForkTsCheckerWebpackPlugin(),
-    ],
+    output: {
+      filename: '[name].[contenthash].js',
+      path: path.resolve(__dirname, '../dist'),
+      clean: true,
+    },
     module: {
       rules: [
         //TypeScript
         {
-          test: /\.ts(x)?$/,
+          test: /\.ts(x?)$/,
           use: [
             {
+              loader: 'thead-loader',
+              options: {
+                workers: 8,
+                poolTimeout: 5000,
+              }
+            },
+            {
             loader: 'ts-loader',
-            // options: {
-            //   transpileOnly: true,
-            //   },
+            options: {
+              transpileOnly: true,
+              },
             },
           ],
           include: path.resolve(__dirname, 'src'),
         },
-        //CSS
-        // {
-        //   test: /\.css$/,
-        //   use: ['style-loader', 'css-loader'],
-        //   include: path.resolve(__dirname, 'src'),
-        // },
-        //SVG
-        // {
-        //   rest: /\.svg$/,
-        //   use: [
-        //     {
-        //       loader: 'svg-url-loader', 
-        //       options: 10000,
-        //     },
-        //   ],
-        // },
+        // CSS
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+          include: path.resolve(__dirname, 'src'),
+        },
         //Images
-        // {
-        //   test: /\.(png|svg|jpg|jpeg|gif)$/,
-        //   type: 'asset/resource',
-        //   include: path.resolve(__dirname, 'src'),
-        // },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/,
+          use: [
+            {
+              loader: 'url-loader',
+            },
+          ],
+          include: path.resolve(__dirname, 'src'),
+        },
         //rest
-        // {
-        //   test: /.*/,
-        //   use: 'file-loader',
-        //   include: path.resolve(__dirname, 'src'),
-        // },
+        {
+          use: 'file-loader',
+          exclude: [/\.(svg|png|bmp|gif|jpg|jpeg|css|scss|sass|js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/],
+        },
       ],
     },
     resolve: {
       extensions: ['.tsx', '.ts', '.js'],
       symlinks: false,
     },
-    output: {
-      filename: '[name].[contenthash].js',
-      path: path.resolve(__dirname, 'dist'),
-      //po co czyscic skryptem jezeli mozna tutaj?
-      clean: true,
-    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Production',
+      }),
+      new ForkTsCheckerWebpackPlugin(),
+    ],
 };
